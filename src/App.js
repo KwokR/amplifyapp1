fc3145d70942f031ad6645b4da88b4609aabea5b
RayKwok33 <raykwok33@gmail.com>
2021-08-26 13:45:45 -0700

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 import Amplify, { PubSub } from 'aws-amplify';
