5a16d69dc85f91e2eab379e74a7321d2e2485a4a
RayKwok33 <raykwok33@gmail.com>
2021-08-26 14:07:46 -0700

    change subscribe position

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ import Amplify, { PubSub } from 'aws-amplify';
 import { AWSIoTProvider } from '@aws-amplify/pubsub/lib/Providers';
 
 Amplify.configure(awsconfig)
+
 Amplify.addPluggable(new AWSIoTProvider({
      aws_pubsub_region: 'us-west-2',
      aws_pubsub_endpoint: 'wss://a351g8i6j3ec9s-ats.iot.us-west-2.amazonaws.com/mqtt',
    }));
+
+PubSub.subscribe('myTopic').subscribe({
+    next: data => console.log('Message received from myTopic', data),
+    error: error => console.error(error),
+    close: () => console.log('Done'),
+});
 
 function App() {
   return (
@@ -25,8 +32,3 @@ function App() {
 
 export default withAuthenticator(App);
 
-PubSub.subscribe('myTopic').subscribe({
-    next: data => console.log('Message received', data),
-    error: error => console.error(error),
-    close: () => console.log('Done'),
-});
