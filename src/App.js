0a12a346d1b1c8ca38fa9cc288bd9de95375170a
RayKwok33 <raykwok33@gmail.com>
2021-08-26 13:34:27 -0700

    added awsiot pubsub

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+import Amplify, { PubSub } from 'aws-amplify';
+import { AWSIoTProvider } from '@aws-amplify/pubsub/lib/Providers';
 
 Amplify.configure(awsconfig)
+Amplify.addPluggable(new AWSIoTProvider({
+     aws_pubsub_region: 'us-west-2',
+     aws_pubsub_endpoint: 'wss:a351g8i6j3ec9s-ats.iot.us-west-2.amazonaws.com/mqtt',
+   }));
 
 function App() {
   return (
@@ -20,3 +26,8 @@ function App() {
 
 export default withAuthenticator(App);
 
+PubSub.subscribe('myTopic').subscribe({
+    next: data => console.log('Message received', data),
+    error: error => console.error(error),
+    close: () => console.log('Done'),
+});
